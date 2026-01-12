7febde82ab5a8936ee60af4bf955659df2eff585
Alexander <aalbrecht@psi.de>
2026-01-12 14:21:12 +0100

    first model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Board(models.Model):
+    title = models.CharField(max_length=100)
+    member_count = models.PositiveIntegerField(default=1)
+    ticket_count = models.PositiveIntegerField(default=0)
+    tasks_to_do_count = models.PositiveIntegerField(default=0)
+    tasks_high_prio_count = models.PositiveIntegerField(default=0)
+    owner_id = models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.name
+
+class Task(models.Model):
