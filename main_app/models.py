0287910aa33b7a2448b012a5d8df999f884bfd78
AlexanderAlbrecht1 <alexander.albrecht1@gmail.com>
2026-01-16 23:53:01 +0100

    mixed

--- main_app/models.py
+++ main_app/models.py
@@ -12,11 +12,15 @@ class Board(models.Model):
         return self.name
 
 class Task(models.Model):
-    board = models.models.PositiveIntegerField()
+    board = models.PositiveIntegerField()
     title = models.CharField(max_length=200)
     description = models.TextField()
     status = models.CharField(max_length=50)
     priority = models.CharField(max_length=50)
-    assignee_id = models.PositiveIntegerField(null=True, blank=True)
-    reviewer_id = models.PositiveIntegerField(null=True, blank=True)
+    assignee_id = models.PositiveIntegerField(null=True, blank=True) # id wird außer der user liste genommen und die details in der respond angezeigt
+    reviewer_id = models.PositiveIntegerField(null=True, blank=True) # id wird außer der user liste genommen und die details in der respond angezeigt
     due_date = models.DateField(null=True, blank=True)
+    comments_count = models.PositiveIntegerField(default=0)
+
+    def __str__(self):
+        return self.name
