f0591a554df636ea252250582d4960fccab6c1ce
AlexanderAlbrecht1 <alexander.albrecht1@gmail.com>
2026-01-17 22:45:08 +0100

    POST view - respond

--- main_app/api/views.py
+++ main_app/api/views.py
@@ -1,6 +1,13 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
-@api_view()
+@api_view(['GET','POST'])
 def single_board_view(request):
-    return Response({"message": "Hello, world!"})
+    if request.method == 'GET':
+        return Response({"message": "This is a GET request"})
+    if request.method == 'POST':
+        try:
+            data = request.data # hier kommt hin in welchem format die daten eingegebn werden müssen
+            return Response({"new_board_added": data}, status=201)
+        except:
+            return Response({"error": "Invalid data"}, status=400)
